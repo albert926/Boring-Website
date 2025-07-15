23d82b9500218a78537866e08b3eacdcfdf6264e
albert926 <103927053+albert926@users.noreply.github.com>
2025-07-15 11:19:26 +0800

    test

--- login.php
+++ login.php
@@ -1,20 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Grab form inputs
-    $username = htmlspecialchars($_POST['username']);
-    $password = htmlspecialchars($_POST['pswrd'])
-
-    $filePath = "/home/albert/logins.txt";
-
-    // Write to the file
-    if ($file = fopen($filePath, "a")) {
-        fwrite($file, "Username: $username, Password: $password\n");
-        fclose($file);
-    }
-
-    header("Location: welcome.php?username=" . urlencode($username));
-    exit;
-} else {
-    echo "Invalid request.";
-}
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
 ?>
