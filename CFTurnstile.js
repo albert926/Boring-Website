4026f64a18d104983ffddcd0d046e2f355e87248
albert926 <103927053+albert926@users.noreply.github.com>
2025-07-15 13:12:15 +0800

    bruh

--- CFTurnstile.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  turnstile.render('.cf-turnstile', {
-    sitekey: '0x4AAAAAABlI9CNxe5EyiFEo',
-    callback: () => {
-      document.getElementById('loginBtn').disabled = false;
-    }
-  });
-});
