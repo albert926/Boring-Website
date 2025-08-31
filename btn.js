ab5bd0638ebd4c2e548eb08dadf934cdd5085b8d
albert926 <103927053+albert926@users.noreply.github.com>
2025-08-31 19:38:18 +0800

    Sorry guys no more git

--- btn.js
+++ btn.js
@@ -3,10 +3,4 @@ document.addEventListener('DOMContentLoaded', () => {
       window.location.href = 'login.html';
     });
   });
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('gitBtn').addEventListener('click', () => {
-      window.location.href = 'https://git.alberttalkstech.com';
-    });
-  });
-  
+   
